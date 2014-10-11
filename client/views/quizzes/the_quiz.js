53a3f45d81b3b4ee30e181d306e5be146d78b35d
Peter Marko <pemoke@gmail.com>
2014-10-11 12:39:43 +1100

    Radio buttons with unique id

--- client/views/quizzes/the_quiz.js
+++ client/views/quizzes/the_quiz.js
@@ -9,7 +9,36 @@ Template.theQuiz.helpers({
         if (this.questions) {
             var currentQuestion = Session.get('currentQuestion') === undefined ?
                 Session.set('currentQuestion', 0) : Session.get('currentQuestion');
-            return (this.questions[currentQuestion]);
+            //var x = this.questions[0];
+            //return x.choices.map(function(currentVal, index) {
+            //    x.index = index;
+            //    return x;
+            //});
+            ////console.log(x.index);
+            //return (this.questions[currentQuestion]);
+
+            // setup question and choices object
+            var qc = this.questions[currentQuestion];
+
+            // store the  _id of current quiz
+            qc.quizId = this._id === undefined ? null : this._id ;
+
+            // store array index of current question
+            qc.questionIndex = currentQuestion;
+
+            // store the question
+            qc.question = this.questions[currentQuestion].question;
+
+            // get the choices array
+            var choices = this.questions[currentQuestion].choices;
+
+            //remap the choices array to store array's index
+            qc.choicesWithIndex = choices.map(function(value, index) {
+                return {value: value, choiceIndex: index, quizId: qc.quizId, questionIndex: qc.questionIndex};
+            });
+
+            return qc;
+
         }
         // TODO the Session needs to be reset if a different quiz is started
 
